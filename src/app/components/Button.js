0f14f1b4a216d104cd8c96911f71a0b395df3760
Nicholas de Jong <nickacidegamer@gmail.com>
2024-07-12 21:54:07 +0200

    Finish Button

--- src/app/components/Button.js
+++ src/app/components/Button.js
@@ -1,9 +1,10 @@
 'use client';
 import { JetBrains_Mono } from 'next/font/google';
 import styles from './Button.module.css';
+import Link from 'next/link';
 
 const jetbrains = JetBrains_Mono({subsets: ['latin']});
 
 export default function Button(props) {
-    return <button className={`${styles.button} ${jetbrains.className}`}>{props.children}</button>
+    return <div className={styles.grow}><Link href={props.href}><button className={`${styles.button} ${jetbrains.className}`}>{props.children}</button></Link></div>
 }
