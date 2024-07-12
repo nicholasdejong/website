6ff0f6a70af34c50db98fb00f1d8d38765f6cd19
Nicholas de Jong <nickacidegamer@gmail.com>
2024-07-12 21:52:40 +0200

    TODO: Contact

---
+++ src/app/contact/page.js
@@ -0,0 +1,9 @@
+import styles from './page.module.css';
+
+export default function Contact() {
+    return <div className={styles.page}>
+        <article className={styles.article}>
+            TODO
+        </article>
+    </div>
+}
