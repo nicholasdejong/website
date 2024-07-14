44368660d3f1e3b08c0a88afd020c78dfdb0e736
Nicholas de Jong <nickacidegamer@gmail.com>
2024-07-14 18:52:40 +0200

    Add submitted page

---
+++ src/app/submitted/page.js
@@ -0,0 +1,5 @@
+export default function Submitted() {
+    return <div style={{display: 'flex', justifyContent: 'center', alignItems: 'center', flexDirection: 'column', height: '100%'}}>
+        <h1>Thank you for your response.</h1>
+    </div>
+}
