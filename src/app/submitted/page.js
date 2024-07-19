d7205c66dac632ac8b79782a82cc47331ea3c4b1
Nicholas de Jong <nickacidegamer@gmail.com>
2024-07-19 16:30:13 +0200

    Old place for `/contact`, `/`, `/api/submitted`
    I relocated these files to a (header_footer) route group

--- src/app/submitted/page.js
+++
@@ -1,5 +0,0 @@
-export default function Submitted() {
-    return <div style={{display: 'flex', justifyContent: 'center', alignItems: 'center', flexDirection: 'column', height: '100%'}}>
-        <h1>Thank you for your response.</h1>
-    </div>
-}
